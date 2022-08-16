eee2582f30f829921f599a22fb0885edc38dc4cb
obi-hellin <chdo999@naver.com>
2022-08-16 11:06:05 +0900

    자바스크립트로 DOM의 정보를 localstorage에 저장해보고있습니다.

--- vue-extension/public/index.js
+++ vue-extension/public/index.js
@@ -1,27 +1,30 @@
-var TestDIV = document.createElement("div");
-TestDIV.innerHTML = `테스트 중 입니다....<br>
-<img src="long_predi.png" alt="logo">
-<div class="vGraph">
-    <ul class = "GraphZone">
-    <li><span class="gTerm">LSTM</span><span class="gBar" style="height:33%"><span>33%</span></span></li>
-    <li><span class="gTerm">KNN</span><span class="gBar" style="height:20%"><span>20%</span></span></li>
-    <li><span class="gTerm">Log</span><span class="gBar" style="height:30%"><span>30%</span></span></li>
-    <li><span class="gTerm">ANN</span><span class="gBar" style="height:40%"><span>40%</span></span></li>
-    <li><span class="gTerm">XGB</span><span class="gBar" style="height:50%"><span>50%</span></span></li>
-    <li><span class="gTerm">RID</span><span class="gBar" style="height:60%"><span>60%</span></span></li>
-    <li><span class="gTerm">SVM</span><span class="gBar" style="height:30%"><span>80%</span></span></li>
-    <li><span class="gTerm">HMM</span><span class="gBar" style="height:55%"><span>80%</span></span></li>
-    <li><span class="gTerm">ADA</span><span class="gBar" style="height:75%"><span>80%</span></span></li>
-    <li><span class="gTerm">OLS</span><span class="gBar" style="height:44%"><span>80%</span></span></li>
-    </ul>
-    <div class = "PriceZone">
-        신발 가격 : 713,000,000원<br> 예측 가격 : 40,000원
-    </div>
-</div>`;
-TestDIV.setAttribute("id", "TestDIV");
-TestDIV.style.backgroundColor = "white";
+var Brand;
+var Name;
+var ReleaseDate;
+var ReleasePrice;
+var CollabBrand;
 
-var CT = document.getElementsByClassName("column_top");
-CT[0].appendChild(TestDIV);
+Brand = document.querySelector('a.brand').text;
+Name = document.querySelector('p.title').textContent;
+ReleaseDate = document.querySelectorAll('dd.product_info')[1].textContent;
+ReleasePrice = document.querySelectorAll('dd.product_info')[3].textContent;
 
+if (Name.includes(' x ')) {
+    CollabBrand = Name.split(' x ')[1].split(' ')[0]
+} else {
+    CollabBrand = '-'
+};
+
+
+localStorage.setItem('Brand', Brand);
+localStorage.setItem('Name', Name);
+localStorage.setItem('CollabBrand', CollabBrand);
+localStorage.setItem('ReleaseDate', ReleaseDate);
+localStorage.setItem('ReleasePrice', ReleasePrice);
+
+console.log('Brand :', Brand, 
+'CB :', CollabBrand, 
+'Name :', Name, 
+'RD :', ReleaseDate, 
+'RP :', ReleasePrice);
 
