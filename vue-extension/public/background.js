c2eef44a60481aaf2126a900c3ee804e8efe6d1e
obi-hellin <chdo999@naver.com>
2022-08-19 18:43:15 +0900

    Flask 서버에 json 형식의 데이터를 보냅니다.

--- vue-extension/public/background.js
+++ vue-extension/public/background.js
@@ -2,8 +2,21 @@
 chrome.runtime.onInstalled.addListener(async () => {
   console.log(`백그라운드`)
 });
-let SEARCH_KEY = 'KEYWORD'
+
+let SEARCH_KEY;
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // work!
+    sendResponse('background.js에서 받았습니다.')
+    SEARCH_KEY = request
+  }
+);
+
 let RESPONSE_KEY = 'PREDICT_VALUE'
+
+
+
+
 
 function isEmptyObj(obj) {
   if (obj.constructor === Object
@@ -34,9 +47,11 @@ function remove_cmd(key_value) {
     })
   });
 }
+
+
 setInterval(async () => {
   // SEARCH_KEY라는 이름으로 뭔가 저장되었냐?
-  let app = await check_cmd(shoesObject)
+  let app = await check_cmd(SEARCH_KEY)
   // 비워있지 않다면
   if (!isEmptyObj(app)) {
     // 통신 => 여기서 서버쪽으로 딥러닝 예측을 수행할 데이터를 보내면 됩니다.
@@ -44,20 +59,26 @@ setInterval(async () => {
       // 응답 예시 : json
       {message: 'Welcome to Thunder Client', about: 'Thunder Client is a hand-crafted lightweight Rest API Client extension for VS Code.', createdBy: 'Ranga Vadhineni', github: 'github.com/rangav/thunder-client-support', twitter: 'twitter.com/thunder_client', …}
     */
-    fetch('https://www.thunderclient.com/welcome')
-      .then((response) => response.json())
-      .then(async (data) => {
-        // 예측 결과를 저장합니다.
-        console.log(data)
-        console.log({ 'PREDICT_VALUE': data.message })
-        await update_cmd({ 'PREDICT_VALUE': data.message })
-      });
+    console.log(JSON.stringify(SEARCH_KEY));
+    fetch('http://127.0.0.1:5000/', {
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json",
+      },
+      body : JSON.stringify(SEARCH_KEY)
+    })
+    .then((response) => response.json())
+    .then(async (data) => {
+      // 예측 결과를 저장합니다.
+      console.log(data)
+      console.log({ 'PREDICT_VALUE': data.message })
+      await update_cmd({ 'PREDICT_VALUE': data.message })
+    });
     // 키워드 삭제 -> 그래야 통신이 1회만 작동함
-    await remove_cmd([shoesObject])
+    await remove_cmd(SEARCH_KEY)
   }
   else {
     // 저장된 내용이 없어서 아무것도 않한다
-    // console.log( '검색어 없음')
+    console.log( '검색어 없음')
   }
-
 }, 1000)
--- vue-extension/public/site1.js
+++ vue-extension/public/site1.js
@@ -9,7 +9,7 @@ const site_1_init = async () => {
             // 기존 자리 삭제
             //items = document.querySelector('#account')    
             // 예측을 보내기 위한 최소 정보가 로드되었다 -> 특정 요소를 점검
-            let shoesObject = {};
+            var shoesObject = {};
             // Brand
             shoesObject['Brand'] = document.querySelector('a.brand').text;
             // Name
@@ -24,17 +24,43 @@ const site_1_init = async () => {
             } else {
                 shoesObject['Collab'] = 0
             };
+            // 여성용?
+            if (shoesObject['Name'].includes('(W')){
+                shoesObject['(W)'] = 1
+            } else {
+                shoesObject['(W)'] = 0
+            };
+            shoesObject['ImageURL'] = document.getElementsByClassName("image")[0].src;
+
+            console.log(shoesObject);
             if (shoesObject != null) {
                 // 화면에서 정보를 획득하여 서버쪽으로 예측을 요청하기 위한 데이터를 저장합니다.
                 // 화면 조작을 할필요 없다 여기서는 
                 removeAllchild(shoesObject)
                 // 서버로 예측을 보내기 위한 기본 정보 저장
-                chrome.storage.sync.set(shoesObject, () => {
+                chrome.storage.sync.set(
+                shoesObject, async () => {
+                    let temp = await check_cmd(shoesObject)
+                    console.log('---------- 저장된 오브젝트 ----------')
+                    console.log(temp)
                     console.log('검색어 등록 완료')
                     // 반복 타이머 종료
                     clearInterval(tmp)
-                })
-                console.log(chrome.storage.sync.get(shoesObject['Name']))
+                });
+                // background.js 로 메세지와 데이터 전송  
+                chrome.runtime.sendMessage({
+                    Brand : shoesObject.Brand,
+                    Name : shoesObject.Name,
+                    W : shoesObject["(W)"],
+                    ReleaseDate : shoesObject.ReleaseDate,
+                    ReleasePrice : shoesObject.ReleasePrice,
+                    Collabortaion : shoesObject.Collabortaion,
+                    ImageURL : shoesObject.ImageURL,                
+                },
+                    function(response) {
+                        // callback
+                        console.log(response)
+                });
             } else {
                 console.log('해당 요소를 못찾음')
             }
